11378d758dcb84ee898e678ab18d8a0183601ac1
glennanj1 <glennan20@gmail.com>
2021-10-29 14:35:56 -0400

    db schema

---
+++ models/guest.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GuestSchema = new Schema(
+  {
+    name: {type: String, required: true, maxLength: 100},
+    attending: {type: Boolean, required: true},
+    numberOfGuests: {type: Number, required: true},
+    Entree: {type: String, required: true},
+    Dietary: {type: String, required: true, maxLength: 100},
+    guestName: {type: String, required: true},
+    guestMeal: {type: String, required: true},
+    guestDietary: {type: String, required: true, maxLength: 100},
+  }
+);
+
+
+//Export model
+module.exports = mongoose.model('Guest', GuestSchema);
