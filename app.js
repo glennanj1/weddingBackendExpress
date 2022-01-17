21bb95d3b55aaa663374423a80e0ebfbf8a41aea
glennanj1 <glennan20@gmail.com>
2022-01-17 16:52:44 -0500

    cors fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const whitelist = process.env.WHITELISTED_DOMAINS
   : []
 const corsOptions = {
 	origin: function (origin, callback) {
-		if (!origin || whitelist.indexOf(origin) !== -1) {
+		if (origin || whitelist.indexOf(origin) !== -1) {
 		  callback(null, true)
 		} else {
 		  callback(new Error("Not allowed by CORS"))
